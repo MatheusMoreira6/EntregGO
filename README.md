048f3924637885666529f08b147124036fb97c97
Matheus Moreira Mendes <matheus-moreira.ivi75@hotmail.com>
2023-08-31 23:33:03 -0400

    V1.0 - Criação do Projeto

---
+++ README.md
@@ -0,0 +1,6 @@
+# EntregGO
+
+### Trabalho de Conclusão de Curso (TCC)
+
+Aluno: Matheus Moreira Mendes<br>
+RGM: 802.238
---
+++ android/app/src/main/kotlin/com/unigran/engsoftware/entreggo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.unigran.engsoftware.entreggo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
